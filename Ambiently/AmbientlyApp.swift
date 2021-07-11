cfd4ed0e8624b763879b7b0d034404309bbff993
Jude Wilson <jude@alecw.net>
2021-07-11 18:52:15 -0400

    Initial Commit

---
+++ Ambiently/AmbientlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientlyApp.swift
+//  Ambiently
+//
+//  Created by Jude Wilson on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AmbientlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
